2dab7888ec5a40bfc16b436d0765f7130c7a5501
ikhbal <ikhbaldwiyan@gmail.com>
2021-11-04 15:31:53 +0700

    add react icon in for header

--- src/parts/Header.js
+++ src/parts/Header.js
@@ -2,10 +2,17 @@ import React from 'react';
 import Button from 'elements/Button';
 import BrandIcon from 'parts/IconText';
 
+import { AiFillHome } from "react-icons/ai";
+import { RiArticleFill } from "react-icons/ri";
+import { IoGameController } from "react-icons/io5";
+import { GoDeviceDesktop } from "react-icons/go";
+
 export default function Header(props) {
   const getNavLinkClass = (path) => {
     return props.location.pathname === path ? " active" : "";
   };
+
+  const iconHome = {marginBottom: 4};
 
   return (
     <header className="spacing-sm">
@@ -16,22 +23,22 @@ export default function Header(props) {
             <ul className="navbar-nav ml-auto">
               <li className={`nav-item${getNavLinkClass("/")}`}>
                 <Button className="nav-link" type="link" href="">
-                  Home
+                  <AiFillHome style={iconHome} /> Home
                 </Button>
               </li>
               <li className={`nav-item${getNavLinkClass("/article")}`}>
                 <Button className="nav-link" type="link" href="/article">
-                  Article
+                  <RiArticleFill style={iconHome} /> Article
                 </Button>
               </li>
               <li className={`nav-item${getNavLinkClass("/games")}`}>
                 <Button className="nav-link" type="link" href="/games">
-                  Games
+                  <IoGameController style={iconHome} />  Games
                 </Button>
               </li>
               <li className={`nav-item${getNavLinkClass("/tech")}`}>
                 <Button className="nav-link" type="link" href="/tech">
-                  Tech
+                  <GoDeviceDesktop style={iconHome} /> Tech
                 </Button>
               </li>
             </ul>
